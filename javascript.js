ad3e3b6aa06d609a21236a893f22c9798981c6e4
Riils <donamed132@gmail.com>
2024-01-02 10:43:15 +0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log("Genap: " + i);
+// //     } else {
+// //         console.log("Ganjil: " + i);
+//      }
+//  }
+
+// a = 100;
+// while(a >= 1){
+//     if (a % 2 === 0) {
+//         console.log("Genap: " + a);
+//      }
+      
+    
+//     document.write("<span>angka:</span>" + a +'<br>')
+//     a--;   }
