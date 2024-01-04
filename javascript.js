d91a46bfba865bc70364e0887f3fe94c75f6378a
Riils <donamed132@gmail.com>
2024-01-04 16:20:28 +0700

    Bug terus

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 // for (let i = 1; i <= 100; i++) {
 //     if (i % 2 === 0) {
 //         console.log("Genap: " + i);
-// //     } else {
-// //         console.log("Ganjil: " + i);
+//     } else {
+//          console.log("Ganjil: " + i);
 //      }
 //  }
 
